de65871cfec26b88f46a907d99493c2a887d80ba
Cyril Cohen <cohen@crans.org>
2021-03-20 16:24:32 +0100

    revert mkCoqDerivation override

--- config-parser-1.0.0/overlays.nix
+++ config-parser-1.0.0/overlays.nix
@@ -38,9 +38,6 @@ let
   (mk-overlay overlays-dir)
   nixpkgs-overrides
   (self: super: { coqPackages = fold-override super.coqPackages ([
-    (self: super: { mkCoqDerivation = { defaultVersion ? null, ... }@args:
-      super.mkCoqDerivation (args //
-       { defaultVersion = if isNull defaultVersion then "master" else defaultVersion; }); })
     (mk-overlay coq-overlays-dir)
     coq-overrides
     (self: super: { coq = super.coq.override {
