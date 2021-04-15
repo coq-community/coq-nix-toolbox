c72a16f165282e14b6919763c0890151d6d608e3
Cyril Cohen <cohen@crans.org>
2021-04-15 10:40:06 +0200

    adding a test action

--- .nix/fallback-config.nix
+++ .nix/fallback-config.nix
@@ -1,4 +1,25 @@
 {
-format = "1.0.0";
-coq-attribute = "template";
+  ## DO NOT CHANGE THIS
+  format = "1.0.0";
+  ## unless you made an automated or manual update
+  ## to another supported format.
+
+  ## The attribute to build, either from nixpkgs
+  ## of from the overlays located in `.nix/coq-overlays`
+  attribute = "coq-shell";
+  src = ../coq-shell;
+
+  ## select an entry to build in the following `bundles` set
+  ## defaults to "default"
+  default-bundle = "8.11";
+
+  ## write one `bundles.name` attribute set per
+  ## alternative configuration, the can be used to
+  ## compute several ci jobs as well
+  bundles."8.11" = {
+    coqPackages.coq.override.version = "8.11";
+    coqPackages.coq.main-job = true;
+    coqPackages.coq.job = true;
+    coqPackages.coq-shell.job = false;
+  };
 }
