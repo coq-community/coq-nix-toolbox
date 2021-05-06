4a8a287e61ab354b2817e6d626f877b48f490864
Théo Zimmermann <theo.zimmermann@inria.fr>
2021-05-06 16:55:09 +0200

    Update nixpkgs to fix issue with Coq derivation.

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/e8893cc489de8ffbe8125fb0d37f05e8023a2d99.tar.gz;
-    sha256 = "1bsdxz9m50jpirhndkjaqim1vbmnjmhl0y73yqc715m9j9n0l4mi";
+    url = https://github.com/NixOS/nixpkgs/archive/123db833485edf8df83f95ef2888113390768686.tar.gz;
+    sha256 = "1cspmvb0gwh27ifp27pcaqkwl5blsg11samm09csl8h5qlqv3h19";
   }
