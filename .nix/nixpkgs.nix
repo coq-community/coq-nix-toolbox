722b63e4017d54cc686ad6184fcdbecba6ceb04e
Théo Zimmermann <theo.zimmermann@inria.fr>
2021-06-08 16:25:37 +0200

    Update to the latest nixpkgs master.
    Fixes VST.

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/e8893cc489de8ffbe8125fb0d37f05e8023a2d99.tar.gz;
-    sha256 = "1bsdxz9m50jpirhndkjaqim1vbmnjmhl0y73yqc715m9j9n0l4mi";
+    url = https://github.com/NixOS/nixpkgs/archive/01df9c1ae6f9c9c0c452a8469c5d44e2bc4711c0.tar.gz;
+    sha256 = "198095nnvvgj2sr8bzdqirsi386ls0gy8yfwjssmppzxgk4xa0xc";
   }
