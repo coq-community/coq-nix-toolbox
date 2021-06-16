3d8c965afbc3f3948f9042495767f490b98c312a
Théo Zimmermann <theo.zimmermann@inria.fr>
2021-06-16 17:41:10 +0200

    Update nixpkgs after NixOS/nixpkgs#127079.

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/01df9c1ae6f9c9c0c452a8469c5d44e2bc4711c0.tar.gz;
-    sha256 = "198095nnvvgj2sr8bzdqirsi386ls0gy8yfwjssmppzxgk4xa0xc";
+    url = https://github.com/NixOS/nixpkgs/archive/9f78f858f567c94218b4b9a534044c8c7e93e12b.tar.gz;
+    sha256 = "01iiw8m4c29s2vr00vrav8qm5if99b48cw14k8apw0ss1jdrwrc7";
   }
