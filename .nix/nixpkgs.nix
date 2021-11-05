7da2cdbb5b441dff66947667110d348c88fa710a
Pierre Roux <pierre.roux@onera.fr>
2021-11-05 11:33:10 +0100

    coqeal: 1.0.6 -> 1.1.0

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/054bac1eca0f242f226bace4568a4f98df180c7d.tar.gz;
-    sha256 = "06xf8799b899cyv9p1iw5cz0bp8r5z0gc6z7cqz6npqiq5zgd6ns";
-  }
+         url = https://github.com/proux01/nixpkgs/archive/451a053092905e58558372c99a5dbb3d79bbbb2d.tar.gz;
+         sha256 = "1x3jj4kqpw710m0iv0rgykd5wxfdhxnrv05qj38x4zmyy30cwr4l";
+       }
