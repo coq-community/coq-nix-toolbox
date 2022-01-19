46f622d93382efd66926b6bec259baa11671bb3e
Cyril Cohen <cohen@crans.org>
2022-01-19 14:31:20 +0100

    Testing nixpkgs PR 155635

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/c420487e0bfe1cac628adb8477e24778a74b7f23.tar.gz;
-    sha256 = "0c3as3nd8q1ic2g7q58rmxapbidg0yii79y39nc6yksd56m32l8a";
-  }
+         url = https://github.com/CohenCyril/nixpkgs/archive/d75ee1adbd24662cb7937701b81e00285aac659b.tar.gz;
+         sha256 = "0dp846fkyasj1w54aa9lik5n31k1b554kk9gps3vyr8ffx0p8k83";
+       }
