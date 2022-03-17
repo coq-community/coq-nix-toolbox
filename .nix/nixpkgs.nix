0abd99b21408112a891028667d3df51dd96d0c2c
Cyril Cohen <cohen@crans.org>
2022-03-17 22:32:35 +0100

    Testing trackt

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/55283f4f94e8a80e4044ee0f33b019afe5e0aafa.tar.gz;
-    sha256 = "0j66xj12d633fzyv9knc8766y7ay5i7yravqbhz0ji20vgs4y1zm";
-  }
+         url = https://github.com/siraben/nixpkgs/archive/b399da94d15be604e76efd8760b97352dd00aa72.tar.gz;
+         sha256 = "0krkmm3pcyzf9vdwanib8524s669qn5cmk3c4vf5s9py9m06sacp";
+       }
