0e85985ad1427f6143cc116a1ec23728ccd7b619
Pierre Roux <pierre@roux01.fr>
2022-06-09 00:27:28 +0200

    Test https://github.com/NixOS/nixpkgs/pull/176929

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/7ca27a436a023d1adbd2f5687f7bb481c1b92fa0.tar.gz;
-    sha256 = "1wvmlzyk694aakxmcp2g2smbmdbq5lfmljqczwdkp2avkbnkzac7";
+    url = https://github.com/NixOS/nixpkgs/archive/c1daec8d83b4068302a74e8177104ce7f9802d43.tar.gz;
+    sha256 = "07vqhgs560qa9klv0mi1mx7iw9pl6cihrbigkj7vw2fgk5a2pr6p";
   }
