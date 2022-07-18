552c56e56a9528ae541c4846dce6e6b6558385b3
Pierre Roux <pierre.roux@onera.fr>
2022-07-18 17:22:39 +0200

    Update nixpkgs for coq-elpi.1.15.1
    Now that https://github.com/NixOS/nixpkgs/pull/182066 is merged

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/6537fa2abb2f9e28e8f2c2768b853e386619b61f.tar.gz;
-    sha256 = "0gy32kgbaqr82d0irn262a7nnsnndh21kq1svm02ibk8aafw2x4b";
+    url = https://github.com/NixOS/nixpkgs/archive/96be7f5a908ab85b765308665d7f0fa26d4d9d1e.tar.gz;
+    sha256 = "0w289hhjhscr9mxikqmmhlskg1mkkz68aj4268wazs9qvig7l5gw";
   }
