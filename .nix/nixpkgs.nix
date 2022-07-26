53b132495f71fa262ca4b2ce46072cc282587b56
Cyril Cohen <cohen@crans.org>
2022-07-26 15:07:05 +0200

    testing nixpkgs

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/6537fa2abb2f9e28e8f2c2768b853e386619b61f.tar.gz;
-    sha256 = "0gy32kgbaqr82d0irn262a7nnsnndh21kq1svm02ibk8aafw2x4b";
-  }
+         url = https://github.com/vbgl/nixpkgs/archive/b5d0601499c611dc7b71beb13aeaa67c99bf23cb.tar.gz;
+         sha256 = "0gzpxpx7pri3q2n7mcjl15lj6vj6sbqjj6mw8j9z6yld72habnz1";
+       }
