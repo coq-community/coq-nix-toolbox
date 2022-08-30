08d8e5373ec57085a2f8c37ef29f60679ea1d1ec
Cyril Cohen <cohen@crans.org>
2022-08-30 22:18:31 +0200

    Testing analysis 0.5.3

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/96be7f5a908ab85b765308665d7f0fa26d4d9d1e.tar.gz;
-    sha256 = "0w289hhjhscr9mxikqmmhlskg1mkkz68aj4268wazs9qvig7l5gw";
-  }
+         url = https://github.com/vbgl/nixpkgs/archive/d416f196bc2e836eabf829c08a77c85fbbc037f9.tar.gz;
+         sha256 = "1wn48hjjplb9cmr7kc6als1fyifv4v91kl5zpi60f7x3sr38fhz6";
+       }
