5cf6e99fd960e6a32413e71b062500666533ca4f
Théo Zimmermann <theo.zimmermann@inria.fr>
2022-10-01 19:05:07 +0200

    Test Dune 3 update in mkCoqDerivation.
    NixOS/nixpkgs#193942

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/8039b055f8ef0693724a9909145f3daf5d762da5.tar.gz;
-    sha256 = "0fj4mqlnax8ivggs40fc1hkybv49f78nwiy6ikwmqr4sqkk50dxp";
-  }
+         url = https://github.com/Zimmi48/nixpkgs/archive/b7e56cea358fdff780b66227c31a48c4c98c0570.tar.gz;
+         sha256 = "0bk15mbyvif11x6vr2r0gz8bhfpc6a1zh5qjbwvd9ngv18km63jw";
+       }
