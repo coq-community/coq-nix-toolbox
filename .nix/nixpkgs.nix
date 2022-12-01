2e42fbfd29ad6b5ac62be04070e7d4f1ae9b1e5c
Cyril Cohen <cohen@crans.org>
2022-12-01 22:39:49 +0100

    testing coq 8.16.1

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/7e5e6625e2c9c35942e0f0a97fee18cd53991187.tar.gz;
-    sha256 = "1sq77iprkyl0b6l1vylvvbk7mnvka9kgp229zz3vb53gn3967n1w";
-  }
+         url = https://github.com/vbgl/nixpkgs/archive/b5811a67585de95b0412a6550e6b108f23277851.tar.gz;
+         sha256 = "0v91z5gnwawm924ih3liyzdi0n9r1yba5mbpf1hv0jg6ppcgl0fc";
+       }
