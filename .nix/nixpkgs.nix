a1967da0cfba068d7aa56d5ada69bf975151744a
Pierre Roux <pierre.roux@onera.fr>
2023-01-09 10:32:51 +0100

    Update nixpkgs
    To have Coq 8.17+rc1, following the merge of https://github.com/NixOS/nixpkgs/pull/209145

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/7e5e6625e2c9c35942e0f0a97fee18cd53991187.tar.gz;
-    sha256 = "1sq77iprkyl0b6l1vylvvbk7mnvka9kgp229zz3vb53gn3967n1w";
+    url = https://github.com/NixOS/nixpkgs/archive/58b28fcb9ab4473d63322423d72364f57931b9ac.tar.gz;
+    sha256 = "1dv1x8yyqwvz8hd0z260nq5d05mm6jgbw08d3ijzfcql8l6q0lab";
   }
