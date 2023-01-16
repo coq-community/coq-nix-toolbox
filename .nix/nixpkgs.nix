9af0a84ed0c5edbe75f6ba3a0f7317eb22970309
Cyril Cohen <cohen@crans.org>
2023-01-16 21:07:17 +0100

    Testing graph-theory-planar

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/4e9e5aeafeb41f780932097b6935087793b0ce06.tar.gz;
-    sha256 = "05n22m4y96x1qxjcfi4aj12x6724cjdiy04631bvadphs8mrk15s";
-  }
+         url = https://github.com/CohenCyril/nixpkgs/archive/28b9922d6ee2b6fcde3a93d4f92c25b9d7a3f0ed.tar.gz;
+         sha256 = "009kgh0vrk4rx7p0dqqi3lknb64rj61hxyhk98jckgwi9ra4p380";
+       }
