d9b6d3f641589ac7eee7850d02ce70f8d626d277
Vincent Laporte <Vincent.Laporte@gmail.com>
2023-03-03 09:43:34 +0000

    Test nixpkgs PR 217810

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/4e9e5aeafeb41f780932097b6935087793b0ce06.tar.gz;
-    sha256 = "05n22m4y96x1qxjcfi4aj12x6724cjdiy04631bvadphs8mrk15s";
+    url = https://github.com/NixOS/nixpkgs/archive/c45cbad28c3ed663d4fac2e13f2be318efdb6d10.tar.gz;
+    sha256 = "sha256:04747vbr6s5ibhb8bg6hjny8fs36wpnf0h6z5iaarglskm1cavq7";
   }
