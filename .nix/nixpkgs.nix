44a6f50589525d6774c7eab7c30bc020e1c27018
Vincent Laporte <Vincent.Laporte@gmail.com>
2023-03-03 11:53:32 +0000

    mathcomp-analysis-0.6.1

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/c45cbad28c3ed663d4fac2e13f2be318efdb6d10.tar.gz;
-    sha256 = "sha256:04747vbr6s5ibhb8bg6hjny8fs36wpnf0h6z5iaarglskm1cavq7";
+    url = https://github.com/NixOS/nixpkgs/archive/4cd56b36a98e88d8211af4fcfd49743016281409.tar.gz;
+    sha256 = "sha256:10gb23q0z7qsf38xn375crpi7v0fv36bjfxiz5xl8xc5lm1prkjr";
   }
