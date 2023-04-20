a647faf68616c471a44f8c7526df0651f9a42cb0
Pierre Roux <pierre.roux@onera.fr>
2023-04-20 10:29:34 +0200

    Update Nixpkgs

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/6302147d48c5b6b95e492cf9c8e962c58059331c.tar.gz;
-    sha256 = "00sni2k9jrq2h0i801d7y7fz3a6fi4danf0zs1n85hlskl2g87bm";
-  }
+         url = https://github.com/vbgl/nixpkgs/archive/5016f9ab0ad5c3b44925ee5fee254414fe3e7483.tar.gz;
+         sha256 = "0bdn0f668kgiax9r9w34cxaq8p3h04l8vjfwgisvy4r35yv9ky50";
+       }
