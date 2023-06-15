e34f4814dce658f813c39ebe88f6628d6f57b690
Cyril Cohen <cohen@crans.org>
2023-06-15 15:09:11 +0200

    testing nixpkgs PR

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/499cad7a722caf0bebb2a382a67fa39c992acebd.tar.gz;
-    sha256 = "1mbsqk3wgpy23bhpzg6isnbpy44c757830bgja3gzidp0glzxd9j";
+    url = https://github.com/NixOS/nixpkgs/archive/50847470ba43e608d4b77c406ca26313229a49a0.tar.gz;
+    sha256 = "0vk8r3ximc2wk6f1fhqrj7inax5n0z12namh249kgqr60rfhdj96";
   }
