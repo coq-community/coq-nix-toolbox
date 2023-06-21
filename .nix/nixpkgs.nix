c9fc2a9b2fa250ddbff17c0547acec6f7c12d817
affeldt-aist <33154536+affeldt-aist@users.noreply.github.com>
2023-06-21 20:27:51 +0900

    Test infotheo 0.5.2 (#152)
    * add infotheo 0.5.2
    
    * rebase wrt upstream, update after nixpkgs merge

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/50847470ba43e608d4b77c406ca26313229a49a0.tar.gz;
-    sha256 = "0vk8r3ximc2wk6f1fhqrj7inax5n0z12namh249kgqr60rfhdj96";
+    url = https://github.com/NixOS/nixpkgs/archive/3f0d72f755b5bb6357e871ed59db4500345294e5.tar.gz;
+    sha256 = "0qrykx9mgbpmiwrdsvppaahqmcb6iv42vzs51xdwfcjlrbdpazlm";
   }
