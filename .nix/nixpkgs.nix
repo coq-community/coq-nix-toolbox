25b5b43bf81d8c90570d195d28ee0504cb8c6902
Vincent Laporte <Vincent.Laporte@gmail.com>
2023-08-28 14:45:10 +0200

    Update coq-elpi to 1.18.0

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/2ae7493cb6fe1e5d3f3c10a24a7de2ec4213f770.tar.gz;
-    sha256 = "169s7n6yn28k0lkd1cj8ymz321wb4qcp82gaknd6s20azqgvn4ah";
+    url = https://github.com/NixOS/nixpkgs/archive/e8083f3021054cdba15876fd26b6e0b417dc3395.tar.gz;
+    sha256 = "0gp5ws498pdw6zmqk0msfrvppg9c5ggqy8v9pnc132akkv1vvd5x";
   }
