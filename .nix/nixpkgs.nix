d44dcd635dcfe768c2fe5fa69c6f25d46787e242
Vincent Laporte <Vincent.Laporte@gmail.com>
2023-10-13 07:26:41 +0200

    Update: stdpp 1.9.0 and iris 4.1.0
    After NixOS/nixpkgs#260683

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/7c33c8d4529c5f40237c39d83ea4a6e2b32d50a7.tar.gz;
-    sha256 = "03q3ldjzmqq30bzlgyj9n1r7sx8dy4w9h3mz07p6vqwr4v24zhgh";
+    url = https://github.com/NixOS/nixpkgs/archive/8de5195ec427bf7f90b0e64aa7a750bf5e18e4c7.tar.gz;
+    sha256 = "0vgkqfhbcnbk7g0f48zz0jv5mg1sahgc0xnqjv9zwal0vdzjlidn";
   }
