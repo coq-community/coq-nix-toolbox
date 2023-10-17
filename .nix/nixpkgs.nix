5efbe7f00c979ee1cad209f2785da04ade03b4c1
Théo Zimmermann <theo.zimmermann@telecom-paris.fr>
2023-10-17 14:02:30 +0200

    Test deriving update.
    NixOS/nixpkgs#258038

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/8de5195ec427bf7f90b0e64aa7a750bf5e18e4c7.tar.gz;
-    sha256 = "0vgkqfhbcnbk7g0f48zz0jv5mg1sahgc0xnqjv9zwal0vdzjlidn";
-  }
+         url = https://github.com/arthuraa/nixpkgs/archive/5195e9326f3364c50147ffce6998b89fae2a8556.tar.gz;
+         sha256 = "02jwgv96gn26q196lvfh4gfa5r5830d1s513a9aj4rg91572ai4d";
+       }
