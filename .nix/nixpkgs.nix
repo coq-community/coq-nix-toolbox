0ab9d0c52420d56e8c3be438b9efdb299c969d1f
Vincent Laporte <Vincent.Laporte@gmail.com>
2023-10-25 10:03:51 +0200

    Update after VST 2.12
    NixOS/nixpkgs#263298

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/cb5678d28d65c762d61863fbc6257371f12df826.tar.gz;
-    sha256 = "01d7bnmmsb4njzsrzscf0829lmw04rwkn7d5ni1765pdwii14m41";
+    url = https://github.com/NixOS/nixpkgs/archive/42bea51aa72520aee227f47c9257875306d0e4c8.tar.gz;
+    sha256 = "1ifp97mb0g3d8l1q3m9mfbmyl7drnzm8kndx7svj946cjrd1306d";
   }
