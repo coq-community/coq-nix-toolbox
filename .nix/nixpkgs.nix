51a2829fd480952b26a04936db684cd84edabb6e
Pierre Roux <pierre.roux@onera.fr>
2023-11-02 12:08:33 +0100

    MathComp 1.17 -> 1.18
    Now that https://github.com/NixOS/nixpkgs/pull/265046 is merged.

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/e06c716ef149f466f25e62a836c30b90476e65e5.tar.gz;
-    sha256 = "0a8pxwnjddqliswy54f5c6hzw1xip4rpcrgd0222krb49pr39zgm";
+    url = https://github.com/NixOS/nixpkgs/archive/b28c9ac0a4ecb8b978e71555ca52e604b5802e2d.tar.gz;
+    sha256 = "05dw50ns1gq6mx59pqbgp1ail314jrh52vz8300m5zyvlklxb5v9";
   }
