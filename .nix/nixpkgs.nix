447fd9da2820c66dee263a327d62f3f3e9da2688
Vincent Laporte <Vincent.Laporte@gmail.com>
2023-11-07 15:56:16 +0100

    interval 4.9.0; iris-named-props
    Update after NixOS/nixpkgs#266082 and NixOS/nixpkgs#263100

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/42bea51aa72520aee227f47c9257875306d0e4c8.tar.gz;
-    sha256 = "1ifp97mb0g3d8l1q3m9mfbmyl7drnzm8kndx7svj946cjrd1306d";
+    url = https://github.com/NixOS/nixpkgs/archive/e06c716ef149f466f25e62a836c30b90476e65e5.tar.gz;
+    sha256 = "0a8pxwnjddqliswy54f5c6hzw1xip4rpcrgd0222krb49pr39zgm";
   }
