e1d6d1fd976e3d8fa617be52a71179fc38859d71
Vincent Laporte <Vincent.Laporte@gmail.com>
2023-11-13 08:15:10 +0100

    Test Camlp5 update

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/e06c716ef149f466f25e62a836c30b90476e65e5.tar.gz;
-    sha256 = "0a8pxwnjddqliswy54f5c6hzw1xip4rpcrgd0222krb49pr39zgm";
-  }
+         url = https://github.com/vbgl/nixpkgs/archive/b9d5d6f541d56efd9a4c3414f9375de175a34df8.tar.gz;
+         sha256 = "025wnn5c3y7kc7axcavkkzmgm5xlp5xk7i4mciknwbnf4p4i6ak4";
+       }
