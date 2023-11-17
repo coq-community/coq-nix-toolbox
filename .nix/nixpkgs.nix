71d6279e9d375343aaff640fa826f3469c02958c
Vincent Laporte <Vincent.Laporte@gmail.com>
2023-11-17 14:12:54 +0100

    Update (gappalib, dpdgraph, ITree)

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/b28c9ac0a4ecb8b978e71555ca52e604b5802e2d.tar.gz;
-    sha256 = "05dw50ns1gq6mx59pqbgp1ail314jrh52vz8300m5zyvlklxb5v9";
+    url = https://github.com/NixOS/nixpkgs/archive/606d32f3579d8c92c79c10a57d19511279ed2147.tar.gz;
+    sha256 = "0iz2zr2qs2c27hq5pjc0avy4byc176zskhmlbag2c357mzqrcgwy";
   }
