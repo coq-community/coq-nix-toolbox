194ba15e51052402c5657c989a20b93a147b075f
Vincent Laporte <Vincent.Laporte@gmail.com>
2023-11-27 06:57:40 +0100

    Update VST to 2.13
    Follows NixOS/nixpkgs#268659

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/606d32f3579d8c92c79c10a57d19511279ed2147.tar.gz;
-    sha256 = "0iz2zr2qs2c27hq5pjc0avy4byc176zskhmlbag2c357mzqrcgwy";
+    url = https://github.com/NixOS/nixpkgs/archive/271a11a2d6145f0a4c842779599251e8efcb3d48.tar.gz;
+    sha256 = "0yscgic8cbqcs8y0vz1r3w8k11nvdk69gjs9y5fpdg2s3r9rs0v9";
   }
