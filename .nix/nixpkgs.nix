3492fa022c4cd7c00748e4c78476ed6d75861804
Vincent Laporte <Vincent.Laporte@gmail.com>
2023-11-28 05:59:48 +0100

    Update gaia, hydra-battles & gaia-hydras
    After nixos/nixpkgs#270560

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/76c5738ef231b19072cd7b1c0fa6d65cb61b1208.tar.gz;
-    sha256 = "05zny1y4h5zw1ndvgwm148sppifjnbi3sv0gks17k4fpk1d4pshl";
+    url = https://github.com/NixOS/nixpkgs/archive/0881edb66aa4b27e5894898bb2844aee79a5761a.tar.gz;
+    sha256 = "1can8i7rrbsa1c5946amwk8annfp1z3p63b5w0k0wdcc8y4237h9";
   }
