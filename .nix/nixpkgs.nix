1c417b2dd8e09f7c2380abbde087c8136ea62232
Vincent Laporte <Vincent.Laporte@gmail.com>
2023-12-08 13:00:26 +0100

    Update coq-ext-lib to 0.12.0

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/6225a5106080e3c6e150315e0b55c190ccd5044c.tar.gz;
-    sha256 = "16rd9qkpmn3a5b7y41497i5ifzkpmmrp0zmvkv7hpgfnd068nfac";
+    url = https://github.com/NixOS/nixpkgs/archive/eeee184c00a7e542d2a837252a0ed4e74dd27dc5.tar.gz;
+    sha256 = "17zj05a1g2nz87v2791bha6x64l6h0ny9nplaf468164bj4w49q6";
   }
