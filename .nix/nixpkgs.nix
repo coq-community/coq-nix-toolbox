d1af15bbb32565b47d89427af59c0f573f61a385
Cyril Cohen <cohen@crans.org>
2024-01-11 13:21:18 +0100

    Update to nixpkgs master

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/4a5aaba114d6d533b86054b9a88757adad4e3029.tar.gz;
-    sha256 = "197dwhnc1naz0v0wnq7ygd2cx0qwp48hzm9qzhsq8542y3ak0adv";
+    url = https://github.com/NixOS/nixpkgs/archive/0e2fe90b67468d733694ac558c87139e3512960c.tar.gz;
+    sha256 = "0c0p5nlhic4xiynpm28zpn38jv8cck69mq4gsxqbzmkbhkaf6v8d";
   }
