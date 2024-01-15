1a93a9ac140dd95dc4374a6bb7f11dbd5a0bf9a3
Pierre Roux <pierre.roux@onera.fr>
2024-01-15 15:19:39 +0100

    Add mathcomp 1.19.0

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/4a5aaba114d6d533b86054b9a88757adad4e3029.tar.gz;
-    sha256 = "197dwhnc1naz0v0wnq7ygd2cx0qwp48hzm9qzhsq8542y3ak0adv";
+    url = https://github.com/NixOS/nixpkgs/archive/e1f1b5bcd2db477bbb5e76439154400cdc6590c1.tar.gz;
+    sha256 = "11k29k36y4y8fcyazwazq6nryvnx2v3cpgmnc0ry6g6ks7n1vmyz";
   }
