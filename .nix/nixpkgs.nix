e364cea55507d79d63155247f2ad394f555348c6
Sebastian Ertel <sebastian.ertel@gmail.com>
2024-01-31 16:08:52 +0100

    changes for nixpkgs PR 285276
    adapting the mathcomp-infotheo versions
    
    currently no support for mathcomp-infotheo 0.6.0
    
    relaxing infotheo constraints
    
    infotheo updates: bounding mathcomp-analysis to version 0.7.0 and supporting the new version 0.6.1 that resolved the hierarchy-builder dependency.

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/3ada8a22ba0673d7284f5a269c0238943d254f8f.tar.gz;
-    sha256 = "0mk9riqzy39zpqjbblbi8rcnhnbhsv1l011c55w7d3sic1194101";
-  }
+         url = https://github.com/proux01/nixpkgs/archive/cb726882ae90af468e9bb21b74c4d0bd9ebc3d87.tar.gz;
+         sha256 = "1mzyj8jfghl96mxwh6ji7jai8vd6v6kh97d0kdmc0m07aky698pg";
+       }
