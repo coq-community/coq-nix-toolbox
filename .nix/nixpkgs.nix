5801ba0218014e56490863143ba0d5e5febe097f
Pierre Roux <pierre@roux01.fr>
2024-02-08 20:29:16 +0100

    infotheo not available for mathcomp2

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-         url = https://github.com/proux01/nixpkgs/archive/cb726882ae90af468e9bb21b74c4d0bd9ebc3d87.tar.gz;
-         sha256 = "1mzyj8jfghl96mxwh6ji7jai8vd6v6kh97d0kdmc0m07aky698pg";
+         url = https://github.com/proux01/nixpkgs/archive/fe5fed5f9450ac0ff4acc809e8bdf00a6cb25b3e.tar.gz;
+         sha256 = "1c6z5gra706nwx7yd0b9xn2yi8wrmlagi6r9835lzwwcra7hgmjc";
        }
