68e8793b356a5769188e47722090660809119d4a
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-02-09 15:28:04 +0100

    Update

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-         url = https://github.com/proux01/nixpkgs/archive/fe5fed5f9450ac0ff4acc809e8bdf00a6cb25b3e.tar.gz;
-         sha256 = "1c6z5gra706nwx7yd0b9xn2yi8wrmlagi6r9835lzwwcra7hgmjc";
+         url = https://github.com/sertel/nixpkgs/archive/4ccd0ac4aec0f5750ebd6af7b251eb0fbb1319ec.tar.gz;
+         sha256 = "1k542vlrqd6pk8bxh21686cqka78aayyn4cmkb6wbgcbwg8lsaci";
        }
