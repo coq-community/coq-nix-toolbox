23875c845409c163754826f19fa7b50a7c02a698
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-03-22 08:25:50 +0100

    mathcomp-word: 2.2 → 2.3; 3.0 → 3.1

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/96df16fbd374aa621797505ea5a74244711b0670.tar.gz;
-    sha256 = "0mzcz6qcyfvf8073cyxp33lz8csz7z58p6xpbdj2mq8qgmkhf5cc";
+    url = https://github.com/NixOS/nixpkgs/archive/d70d9425f49f9aba3c49e2c389fe6d42bac8c5b0.tar.gz;
+    sha256 = "1i3phmk47a7f4sp0dlp0ppk0k9w8sskyr112wwc9055c9fn6r0wb";
   }
