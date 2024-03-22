9293930035c8117507d372b38f6765a60950f68c
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-03-22 16:08:58 +0100

    Enable CompCert for Coq 8.18 & Coq 8.19

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/d70d9425f49f9aba3c49e2c389fe6d42bac8c5b0.tar.gz;
-    sha256 = "1i3phmk47a7f4sp0dlp0ppk0k9w8sskyr112wwc9055c9fn6r0wb";
+    url = https://github.com/vbgl/nixpkgs/archive/833e32a2bc0e5e0d37a31551bbc4d1a8b9b051ab.tar.gz;
+    sha256 = "1pn2fg0pcpx19llxfk562p3cc2m9nm9q55lib7n2y0wcb2n14mv1";
   }
