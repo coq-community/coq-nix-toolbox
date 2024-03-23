abb4982c7d47a00005ede4bae2e2c47d895a41dc
Vincent Laporte <vbgl@users.noreply.github.com>
2024-03-23 10:04:55 +0100

    Enable CompCert for Coq 8.18 & Coq 8.19 (#205)
    * Enable CompCert for Coq 8.18 & Coq 8.19
    
    * VST: 2.13 → 2.14

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/d70d9425f49f9aba3c49e2c389fe6d42bac8c5b0.tar.gz;
-    sha256 = "1i3phmk47a7f4sp0dlp0ppk0k9w8sskyr112wwc9055c9fn6r0wb";
+    url = https://github.com/NixOS/nixpkgs/archive/e94ef3d0543fa039b177767c9824ed5847470453.tar.gz;
+    sha256 = "1mfc2bwdlkd6k9x80a3fwx28c3prbz8wq7s2w04ga8wm4z2bjqw1";
   }
