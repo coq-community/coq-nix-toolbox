ea47c74ece9ecd1934194b6f23bb61d023581083
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-03-27 08:27:32 +0100

    Enable HoTT for Coq 8.19

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-         url = https://github.com/vbgl/nixpkgs/archive/0f3493ac3c507b07adea3a911d5d4a1c990228a6.tar.gz;
-         sha256 = "1h56s6wxhrgk8lv85cv3am80ag8jnmcmccpmc45v7d28s4dwvzc2";
+         url = https://github.com/vbgl/nixpkgs/archive/d247c18076f543fb2d96ed450c3ebd08d67ce55e.tar.gz;
+         sha256 = "178g81aswbymdxj07nza7gsnz58p9rj7x24c5hmrxzr64l795v6c";
        }
