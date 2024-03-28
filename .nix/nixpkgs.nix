bc9e46de60b9135f47712bee8146592b2d0210fe
Vincent Laporte <vbgl@users.noreply.github.com>
2024-03-28 07:55:12 +0100

    Enable dpdgraph & HoTT for Coq 8.19 (#206)
    Update after NixOS/nixpkgs#299461

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/e94ef3d0543fa039b177767c9824ed5847470453.tar.gz;
-    sha256 = "1mfc2bwdlkd6k9x80a3fwx28c3prbz8wq7s2w04ga8wm4z2bjqw1";
+    url = https://github.com/NixOS/nixpkgs/archive/e0188a425eb0b221701e56a5e7b028713ffc7b62.tar.gz;
+    sha256 = "1z2gcjczcqn1zsxxj50d66c8ym3aryvv2r2jzcncmayy71m3a9i3";
   }
