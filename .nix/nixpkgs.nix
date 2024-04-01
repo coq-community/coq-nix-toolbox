fd05146bcc239d7e4c38bdaf5b5a8840012d3584
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-04-01 08:33:46 +0200

    interval: 4.9.0 → 4.10.0

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/824952ff6b32b0019465b139b5c76d915ec074ea.tar.gz;
-    sha256 = "1b1j2inyhinwv1aqajykv3magrkkm22aphld2qw31r3hq7v4jvrx";
-  }
+         url = https://github.com/vbgl/nixpkgs/archive/40937bfc360a9ed9513e983204b4bc5b4df0fa34.tar.gz;
+         sha256 = "1dkl9nc5v9waqxkbxm0dhjrp5gnimyh8gp956kx5d45g78bgfl2l";
+       }
