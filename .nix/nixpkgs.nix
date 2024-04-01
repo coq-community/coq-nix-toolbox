3f38217e54b3a51f925dc71c61095a54f59dd215
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-04-01 14:10:14 +0200

    gappalib: 1.5.4 → 1.5.5

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-         url = https://github.com/vbgl/nixpkgs/archive/40937bfc360a9ed9513e983204b4bc5b4df0fa34.tar.gz;
-         sha256 = "1dkl9nc5v9waqxkbxm0dhjrp5gnimyh8gp956kx5d45g78bgfl2l";
+         url = https://github.com/vbgl/nixpkgs/archive/4331f9ca986055ffe9d2c0b8ee65baef4fb50002.tar.gz;
+         sha256 = "0xzzvgzlc9nbssy9g8iw6bycnxkp2kvpi0hna2w2iyysqk8j0xss";
        }
