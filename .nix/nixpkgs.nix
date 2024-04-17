2079878b87e00d21382f097be30361c6ed353417
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-04-17 06:36:09 +0200

    CoLoR: 1.8.4 → 1.8.5

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/1b411ccbf39a1971cdb5bf809806c4c395d1bb24.tar.gz;
-    sha256 = "0z2ahindn18rcay8zbmniirqdxhzda6y9fi1vb91dvfx6s22jw7a";
-  }
+         url = https://github.com/vbgl/nixpkgs/archive/9ee01cfc4654599a76169df6d87402b23598a23c.tar.gz;
+         sha256 = "1ml2ky0g4n078q8xphp296b6wj8af1sbvnkws4b7gzcln168cl79";
+       }
