c6aa0fb73909f47e3bb8c1273e15314322a3077f
Sebastian Ertel <sebastian.ertel@gmail.com>
2024-04-30 15:40:37 +0200

    SSProve does not work with mathcomp 1.18.0.

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-         url = https://github.com/sertel/nixpkgs/archive/67a7104c4faa23ec6a63c34c2186819f1c14bb4b.tar.gz;
-         sha256 = "0j8601szmspxyiahclxxa1ndfiv7q7jims1i1k19v63yz6qnq607";
+         url = https://github.com/sertel/nixpkgs/archive/662a5974cd92faa6eb353f00809c0d0da7ecc817.tar.gz;
+         sha256 = "0hg50i4s3pyk0zjawsrb0a9frvqkxcj19l0zz396749bxwg3c3yw";
        }
