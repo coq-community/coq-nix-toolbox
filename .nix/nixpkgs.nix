a49ac5133ec7a0ca7fc0140fd985c0f7a8099d1b
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-05-03 10:29:12 +0200

    2024-05-03T10:29:12+02:00

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-         url = https://github.com/sertel/nixpkgs/archive/662a5974cd92faa6eb353f00809c0d0da7ecc817.tar.gz;
-         sha256 = "0hg50i4s3pyk0zjawsrb0a9frvqkxcj19l0zz396749bxwg3c3yw";
+         url = https://github.com/sertel/nixpkgs/archive/e829e10293e7c738a900e1a9c9d02d878ec7aa97.tar.gz;
+         sha256 = "1vxq6010lcm1n8rv2f127r9pljmq69qbi2h5vh63fnvax6zyig6n";
        }
