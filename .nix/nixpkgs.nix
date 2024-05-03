59117c351e011a327f2e0674299f95264ab2ed84
Sebastian Ertel <sebastian.ertel@gmail.com>
2024-05-03 13:02:05 +0200

    Adding SSProve to the CI (#213)
    Update after NixOS/nixpkgs#306981

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/c82a0a78f6ad12c7932435221f8542cb12bd4e12.tar.gz;
-    sha256 = "01agzs6pqwgn2ks2f2zh79kiy9y6np4s9qpy1nq064y09yn9bzs0";
+    url = https://github.com/NixOS/nixpkgs/archive/ea51404f4e50c9a2692697f30dae6b2649f953f9.tar.gz;
+    sha256 = "1srv3vcwa3airy7qwcvdn125hi7vw5ppnkmm5pjwdyyi4h049n5f";
   }
