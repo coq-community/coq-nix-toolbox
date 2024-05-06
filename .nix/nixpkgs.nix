f1b09a01e35f96ff345095e45f27cb6672567fa9
Vincent Laporte <vbgl@users.noreply.github.com>
2024-05-06 14:59:30 +0200

    coq-record-update: 0.3.4 (#215)
    Update after NixOS/nixpkgs#309261

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/dd4070b45f7c18fba29ce00ff979c19b389350ae.tar.gz;
-    sha256 = "0xqhsm0i3zsc45d658fi85ckpdycajh87rsmic70cv3k81wn61zc";
+    url = https://github.com/NixOS/nixpkgs/archive/1e9759cb3731963f7007cce49dfa6ff1f412ea0c.tar.gz;
+    sha256 = "06iyj2q23qppzari34vispj6yimj2mfs8vb48f7cbikhflzqjhfc";
   }
