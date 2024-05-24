b1032b288edd31d3d17962421ba10ab564b8193e
Vincent Laporte <vbgl@users.noreply.github.com>
2024-05-24 06:57:48 +0200

    graph-theory 0.9.4 (#217)
    Update after NixOS/nixpkgs#314001

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/1e9759cb3731963f7007cce49dfa6ff1f412ea0c.tar.gz;
-    sha256 = "06iyj2q23qppzari34vispj6yimj2mfs8vb48f7cbikhflzqjhfc";
+    url = https://github.com/NixOS/nixpkgs/archive/8c03ab65690d22027c85220d6e74ef7b028f9e15.tar.gz;
+    sha256 = "06cpyjkcldnv7abfg9apibkjs5c0pbp23is2yr8r8alpwpcah1lk";
   }
