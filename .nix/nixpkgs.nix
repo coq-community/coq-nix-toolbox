cf99e1959f84cecdd24e4d9884a5ab2bb3f74c86
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-06-10 14:06:42 +0200

    coq 8.19.2
    Update after NixOS/nixpkgs#318799

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/47195c4d6939e518b3755ec74995866cc4af27af.tar.gz;
-    sha256 = "1vjsyd4n0c44hz5nsnlqri7b8lirwrqrs0aimsgbimq8ryzwn767";
+    url = https://github.com/NixOS/nixpkgs/archive/f7ff7d4e9b2d589f80ca85a91e4423f76444c2d8.tar.gz;
+    sha256 = "1393hfrl7c7r89kdznwhwgn9mpn6vhff9byvkwrp5qhbb9njhh1x";
   }
