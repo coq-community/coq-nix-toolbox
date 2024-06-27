a4e3d89b4362edccaf65d80d0a9610ccc0ccff07
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-06-27 21:48:32 +0200

    coq-ext-lib: 0.12.0 → 0.12.1

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/90e35645fc623215e9b5a0dca87a0e1aa8b5643f.tar.gz;
-    sha256 = "0kr2hrhg4w3q10v6hjpx42cb6bxx9jsgg5f5v87vvwamqzm0lhy1";
-  }
+         url = https://github.com/vbgl/nixpkgs/archive/8d4f6f981de1b0f0917a9ac0be48f50636971f1a.tar.gz;
+         sha256 = "0jxr35621qv5aflzr1npdsg4pnq5jw3yfvdd8wg0vgzik4p3prp4";
+       }
