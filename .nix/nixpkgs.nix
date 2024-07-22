351b0502207d6f5e77a751c9de469a1b8e6e7980
Pierre Roux <pierre.roux@onera.fr>
2024-07-22 15:00:54 +0200

    enable dev overrides
    Useful for CI

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/681e984f8e3d66f4fd8a94ea28bb7a8b87b27216.tar.gz;
-    sha256 = "1r5cicnyw6iff2jkchj2v1l41m6idypa7z4j8vqqjirdh5kkgw9p";
+    url = https://github.com/NixOS/nixpkgs/archive/d041254022bfff77f8bd23f6edfbcad50e69018c.tar.gz;
+    sha256 = "1z3alahxsp8p1ljschz1rsy2fv2s6j2zd3w7vsh0dpd483xdcs9j";
   }
