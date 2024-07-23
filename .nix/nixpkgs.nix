14429cfc5bd171787920ca0848bffcd281a0c2f2
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-07-23 06:53:22 +0200

    Update after NixOS/nixpkgs#328427

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-         url = https://github.com/vbgl/nixpkgs/archive/74f183f10a046021f8972cea40a09bf98de3b1ba.tar.gz;
-         sha256 = "0cj9ibn961p4xvnskfini0gmr3kyanwj1gbh1i1qn72dpzidml48";
-       }
+    url = https://github.com/NixOS/nixpkgs/archive/681e984f8e3d66f4fd8a94ea28bb7a8b87b27216.tar.gz;
+    sha256 = "1r5cicnyw6iff2jkchj2v1l41m6idypa7z4j8vqqjirdh5kkgw9p";
+  }
