4f93ba70f9e910e88c0f4e3710b23ec8613e3275
Vincent Laporte <vbgl@users.noreply.github.com>
2024-07-31 08:50:20 +0200

    metacoq 1.3.2 (#238)
    Update after NixOS/nixpkgs#329844

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/d041254022bfff77f8bd23f6edfbcad50e69018c.tar.gz;
-    sha256 = "1z3alahxsp8p1ljschz1rsy2fv2s6j2zd3w7vsh0dpd483xdcs9j";
+    url = https://github.com/NixOS/nixpkgs/archive/3d84f9b40eafd73a0c0bc8963e383fb4175430ef.tar.gz;
+    sha256 = "0rjwksxashrvggrrv8yijcl237gg36jkacixsp8h0k2kajv9wyz2";
   }
