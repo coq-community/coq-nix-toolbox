42e07b005b5c12698978a15a281d78c8ea57c356
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-08-01 10:07:05 +0200

    coquelicot: 3.4.1 → 3.4.2
    Update after NixOS/nixpkgs#331486

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/f1c85c2fb9136d00144d0e2d053d2aaeb7757057.tar.gz;
-    sha256 = "0siaah2kj32x4wkln7741sss2j3kldx1gf65f3mrxsqc3qq574c9";
+    url = https://github.com/NixOS/nixpkgs/archive/cca3d59564634572ce82ecae5c9a413e0db174b9.tar.gz;
+    sha256 = "0jvpnvfhavrl2qizwiiv0y6d2rxhjmb6ffancx1aq48bpfz9297n";
   }
