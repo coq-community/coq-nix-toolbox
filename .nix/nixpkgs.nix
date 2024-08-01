27e2bbe350e56eb04a5a837175376dcaeff01125
Vincent Laporte <vbgl@users.noreply.github.com>
2024-08-01 08:39:02 +0200

    flocq: 4.1.4 → 4.2.0 (#239)
    Update after NixOS/nixpkgs#330883

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/3d84f9b40eafd73a0c0bc8963e383fb4175430ef.tar.gz;
-    sha256 = "0rjwksxashrvggrrv8yijcl237gg36jkacixsp8h0k2kajv9wyz2";
+    url = https://github.com/NixOS/nixpkgs/archive/f8932c79253bdff5ae7344019ddc4ea24ea44d9b.tar.gz;
+    sha256 = "1hvq75y0djcacmdiradgvqsj943advh9vw86p1y35mr702x3icla";
   }
