6ddb04d71c1daec7fe7277781b9961949228cde2
Pierre Roux <pierre.roux@onera.fr>
2024-08-02 14:24:02 +0200

    Test coinduction
    Update after NixOS/nixpkgs#331775

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/a69fe54c499fed6e94f9741314002f55588a98c8.tar.gz;
-    sha256 = "007cjffwj2nsq8wr35ivppbzmnmhqif3j6mqmwsfy00pwdvpjzy3";
+    url = https://github.com/proux01/nixpkgs/archive/91346175367b1ca009f23caf6d4d722cbaba3f49.tar.gz;
+    sha256 = "17d9cykswmfqprd1cpq820r41q894dnd5jdpq2h43fbws98ixpcz";
   }
