0c3944888de2071252ffb9a2744f08f7ac06f8b9
Pierre Roux <pierre.roux@onera.fr>
2024-08-07 13:27:37 +0200

    Test stalmarck (#245)
    Update after NixOS/nixpkgs#331760

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/cca3d59564634572ce82ecae5c9a413e0db174b9.tar.gz;
-    sha256 = "0jvpnvfhavrl2qizwiiv0y6d2rxhjmb6ffancx1aq48bpfz9297n";
+    url = https://github.com/NixOS/nixpkgs/archive/a69fe54c499fed6e94f9741314002f55588a98c8.tar.gz;
+    sha256 = "007cjffwj2nsq8wr35ivppbzmnmhqif3j6mqmwsfy00pwdvpjzy3";
   }
