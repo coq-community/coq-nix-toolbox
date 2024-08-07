ee9f82059debae123cf181820d5b83fbf9cf3869
Pierre Roux <pierre.roux@onera.fr>
2024-08-07 17:39:05 +0200

    coqPackages.atbr: init at 8.20.0
    after merge of https://github.com/NixOS/nixpkgs/pull/336491

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/b3a97d4e1fe197b5ce864fe01ea08f886a07f545.tar.gz;
-    sha256 = "0px6rxs8ly1wk5q5qb8l264lraj4a623z6i9czvw5j75glilnbln";
+    url = https://github.com/NixOS/nixpkgs/archive/b512c3409f674b54200b80f9a4c6ca8ad7da4823.tar.gz;
+    sha256 = "0sd1jv9x26cd4qb57p7iaf9m9ksgf0l4rkivnb40433mc6q026kj";
   }
