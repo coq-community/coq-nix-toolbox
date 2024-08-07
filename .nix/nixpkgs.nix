fa956ea00cc67e919f1a7703eb2995fe13aeffe7
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-08-07 13:31:47 +0200

    interval: 4.10.0 → 4.11.0
    Update after NixOS/nixpkgs#332991

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/proux01/nixpkgs/archive/91346175367b1ca009f23caf6d4d722cbaba3f49.tar.gz;
-    sha256 = "17d9cykswmfqprd1cpq820r41q894dnd5jdpq2h43fbws98ixpcz";
+    url = https://github.com/NixOS/nixpkgs/archive/5ef32027f286fb32fb747d26734b7fb50d920c83.tar.gz;
+    sha256 = "0jkfn724igj07gmkvy1mdi8zijh1y1qjm7yj1y1bqbbvai0p0b6q";
   }
