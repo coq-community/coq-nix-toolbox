e7140dd95ad1d0ed3e60a05fe369a7b85d1d2421
Vincent Laporte <vbgl@users.noreply.github.com>
2024-08-20 15:56:00 +0200

    Add MenhirLib & use it in CompCert (#249)
    Update after NixOS/nixpkgs#334922

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/c833fea0fcbececc294ebac1766a966874d15214.tar.gz;
-    sha256 = "0jdkgklb5iiqvkaq6mpj8kmajgjajrgz9l7xh2v0ldic6ihy309r";
+    url = https://github.com/NixOS/nixpkgs/archive/b3a97d4e1fe197b5ce864fe01ea08f886a07f545.tar.gz;
+    sha256 = "0px6rxs8ly1wk5q5qb8l264lraj4a623z6i9czvw5j75glilnbln";
   }
