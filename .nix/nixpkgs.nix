4969ba5c9248a417599edc9a019c09a8757dec4f
Pierre Roux <pierre.roux@onera.fr>
2024-08-27 09:49:26 +0200

    coqPackages.bbv: init at 1.5
    After merge of https://github.com/NixOS/nixpkgs/pull/337663

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/d5fd8a7c6771583b0802d61a1b3709d88876292d.tar.gz;
-    sha256 = "1310s2dz6d6yjaja024i7sf0xmkj7hhd9asvmn40jdxd4gh3p8m5";
+    url = https://github.com/NixOS/nixpkgs/archive/4faf4ca9af3811a0888a1a7d0b9ac600884e9e7e.tar.gz;
+    sha256 = "08rzldkynjqxy6d7vxhpja8jap52q2gwdh58sqb2lqdp8r01mqf8";
   }
