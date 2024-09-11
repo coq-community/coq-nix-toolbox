85fe71740440d17baca09184b3829ab638a4bfe8
Vincent Laporte <vbgl@users.noreply.github.com>
2024-09-11 14:58:36 +0200

    ssprove 0.2.1 (#260)
    Update after NixOS/nixpkgs#327401

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/159be5db480d1df880a0135ca0bfed84c2f88353.tar.gz;
-    sha256 = "0l4fxxbl2akj32nm1fzbxw29jlbnajfd4wmywxz03hk75h44lwrf";
+    url = https://github.com/NixOS/nixpkgs/archive/0e6a2434a572fd583cac02e142ab0689895e395a.tar.gz;
+    sha256 = "01zm3ladh80zh68cjbixiamfwm5m69mf4m4m18brwv3bsxa49vjy";
   }
