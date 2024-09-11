6ac89b15df3422a075111b2e68902957bde8c74c
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-09-11 11:58:19 +0200

    ssprove 0.2.1

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/159be5db480d1df880a0135ca0bfed84c2f88353.tar.gz;
-    sha256 = "0l4fxxbl2akj32nm1fzbxw29jlbnajfd4wmywxz03hk75h44lwrf";
-  }
+         url = https://github.com/4ever2/nixpkgs/archive/963553b30e6498910006dea4cbfb5eee546e7029.tar.gz;
+         sha256 = "0l4hvfsf1yqbdcljz3c5sn9smf8pjpwzhfsrjvspbw5390fwzva4";
+       }
