9035da4111ed193bfc6c20bd8e5d4f1ff8483011
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-09-11 05:56:04 +0200

    Add ElmExtraction
    Update after NixOS/nixpkgs#326305

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/5a18fb6668d569b16c7c8327d7f194d9f38fd413.tar.gz;
-    sha256 = "0mwk1z4fc0xx7yjdy3r0qy1rqicc81dpkqdh904l63gvvna68j0w";
+    url = https://github.com/NixOS/nixpkgs/archive/0d319ecd53b646985675c699deab7097e572e1ac.tar.gz;
+    sha256 = "1w4pkg91rn3wm3hcr6i65j1v1ljqcj5d64k82mnimkzn7l916hh0";
   }
