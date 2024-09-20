b472641a6662a06901b280845b1473e7d6b08ab5
Vincent Laporte <vbgl@users.noreply.github.com>
2024-09-20 13:25:04 +0200

    autosubst 1.9 (#265)
    Update after NixOS/nixpkgs#342990

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/51083570d42e6035b514eb1ddb54a594a46325c8.tar.gz;
-    sha256 = "0s94n1p8g9k83lg0nrxg6nb57rwz8il2a931bi90l55w842kanmm";
+    url = https://github.com/NixOS/nixpkgs/archive/ab606452c41865b9037836cd1d822a9270a992c8.tar.gz;
+    sha256 = "07kxch220njdvqskcj3c572pak05xa2w59i2mv7a1djyb8avwibj";
   }
