4af346fc5b9125df3daad06fba5b2fad6b1e2781
Vincent Laporte <vbgl@users.noreply.github.com>
2024-09-23 10:51:19 +0200

    elpi refactoring (#266)
    Update after NixOS/nixpkgs#343266

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/ab606452c41865b9037836cd1d822a9270a992c8.tar.gz;
-    sha256 = "07kxch220njdvqskcj3c572pak05xa2w59i2mv7a1djyb8avwibj";
+    url = https://github.com/NixOS/nixpkgs/archive/94d0d228173faff39670bc1157ddba8e56012a8d.tar.gz;
+    sha256 = "15g07d3bn68j4xx15sr7mxaf9xcmfkifgfbbdrnjda0spksfhc1h";
   }
