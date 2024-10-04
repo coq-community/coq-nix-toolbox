5dcd430b1af8c87512c19666375bbe0074a27bf6
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-10-04 20:47:44 +0200

    mathcomp-analysis 1.4.0
    mathcomp-infotheo 0.7.2

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/b8b39b395e56ca289e1e4348ef44df6788580331.tar.gz;
-    sha256 = "0r5p19x37xyrpk8d2n0vxfj1r4y563iy5dig25pg65i3d4ayam47";
-  }
+         url = https://github.com/vbgl/nixpkgs/archive/76b7978f2df60b873961ca3014622955bb3c2a5b.tar.gz;
+         sha256 = "0mcrz9lm46228mw7warq9b6aiszr02pmz9pyqsxm0sj0328clbsg";
+       }
