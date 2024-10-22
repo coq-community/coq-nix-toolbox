b576917d3615b94551582d4873fe19b0d47c8458
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-10-22 06:36:50 +0200

    relation-algebra 1.7.10 & 1.7.11

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/6d685d67289afaff9f6a09a05a01cbc825d1bc9c.tar.gz;
-    sha256 = "1h92q7q0sgbknn7pcqp12h3c6qxmii49j05mw8093gfkkrdnjq1q";
-  }
+         url = https://github.com/vbgl/nixpkgs/archive/b5af8badcfc852a6e427a05a9d2ae5233e8576d9.tar.gz;
+         sha256 = "1cz7dggqkixlpc87gisq3wmbrf235svg05khv61niriqb8pw5ian";
+       }
