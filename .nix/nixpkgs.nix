220c9c27e3fe1a01f09d20f83eb3f47ad52c6966
Vincent Laporte <vbgl@users.noreply.github.com>
2024-10-24 17:37:03 +0200

    relation-algebra 1.7.10 & 1.7.11 (#277)
    Update after NixOS/nixpkgs#350405

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/6d685d67289afaff9f6a09a05a01cbc825d1bc9c.tar.gz;
-    sha256 = "1h92q7q0sgbknn7pcqp12h3c6qxmii49j05mw8093gfkkrdnjq1q";
+    url = https://github.com/NixOS/nixpkgs/archive/23ca692e3683f5c098c3879f5c3609e773cfe030.tar.gz;
+    sha256 = "0srx766qc4kww61x6zg8rfyr22rxwmw2k3wkqp5j0m8sv8khgprn";
   }
