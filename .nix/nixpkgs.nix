89527ab4098db2cd956d997e8426f756d074af90
Vincent Laporte <vbgl@users.noreply.github.com>
2024-10-28 08:02:51 +0100

    mathcomp-analysis 1.5.0 (#273)
    Update after NixOS/nixpkgs#351111

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/23ca692e3683f5c098c3879f5c3609e773cfe030.tar.gz;
-    sha256 = "0srx766qc4kww61x6zg8rfyr22rxwmw2k3wkqp5j0m8sv8khgprn";
+    url = https://github.com/NixOS/nixpkgs/archive/fc7f8bb8e6d0bac7e98b1d25ca7734d432d98ff1.tar.gz;
+    sha256 = "1d54nfraaz9qsw9950ncbg9jc7w6jfk62amdk57asz8pshfxjgk8";
   }
