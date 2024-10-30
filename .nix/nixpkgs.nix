33c9b5790ebc3fefa22b326b0c72c4697f52e18e
4ever2 <3417013+4ever2@users.noreply.github.com>
2024-10-30 15:44:01 +0100

    Test MetaCoq

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/05e3b3d344c2bb880135da1b1c29be318af330be.tar.gz;
-    sha256 = "16dskf4w99b55dz9w36g567lv49jvrsqp1dfl0paczy6pafpagbi";
+    url = https://github.com/NixOS/nixpkgs/archive/88f31ac8837ab15748102da8bbc6e96a9330be0d.tar.gz;
+    sha256 = "05bbc8cak5fbb2xic3zr6j54sivg5pra8567zs33ync85scrfgah";
   }
