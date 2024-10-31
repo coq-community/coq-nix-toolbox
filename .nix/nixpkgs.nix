e6f884ad4beae7d6f237ea7bb036badb1b19afdc
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-10-31 14:11:58 +0100

    stdpp 1.11; iris 4.3

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/88f31ac8837ab15748102da8bbc6e96a9330be0d.tar.gz;
-    sha256 = "05bbc8cak5fbb2xic3zr6j54sivg5pra8567zs33ync85scrfgah";
-  }
+         url = https://github.com/vbgl/nixpkgs/archive/f9c95e2357b0e7d1be8b91becd61d17e08e814a1.tar.gz;
+         sha256 = "0m6qn2cnk333gq651dn5qj2i2caf7zch7ns11b21z62a04y6j3aj";
+       }
