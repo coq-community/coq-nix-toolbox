76e87de04598fcf32c101648c8c4ee381ed8cff1
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-11-06 13:15:48 +0100

    ExtLib 0.12.2 (rename from coq-ext-lib)

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/277211f7556b8dfb803e6d6c0d6323064b78d4ec.tar.gz;
-    sha256 = "0pld6dh14cgr1amyckvg8nka7miqgm9k27h2yha0l3c20660pc8z";
-  }
+         url = https://github.com/vbgl/nixpkgs/archive/9512f0f1322740bba0fc013a41259e1b29a9ce21.tar.gz;
+         sha256 = "07pwk2y4686h8ha6z2ckwaval8kv4s255hqc62yh1ckxdkswxdar";
+       }
