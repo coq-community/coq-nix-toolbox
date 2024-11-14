26d68edbd7a4f0c9a7abbafd03cdd519ddeae8d1
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-11-14 21:26:18 +0100

    Fix coq-lsp/serapi

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/7a9a124cad78b449bd02b142582534d12b013cf9.tar.gz;
-    sha256 = "0nr48q2apq7zdvxa6v5mh835nai4wb30vc7bmdzsm7ahwcf9bqm2";
-  }
+         url = https://github.com/vbgl/nixpkgs/archive/c459597ac0620939ee9debeea0bfb740168ae958.tar.gz;
+         sha256 = "0v7fkhvf2z9c91kb2dwks0di7vz0cy6rmfqnv3zlvyml29v2844j";
+       }
