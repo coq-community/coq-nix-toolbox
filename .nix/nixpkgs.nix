373f46c84f8c7b539231ecd6af2f7b69de945d5c
Vincent Laporte <vbgl@users.noreply.github.com>
2024-11-20 07:08:06 +0100

    dpdgraph 1.0+8.20 (#285)
    Update after NixOS/nixpkgs#356290

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/47880618c18d19c705e9fee000e1abf5e7bdd8cd.tar.gz;
-    sha256 = "0akm6flrb10mgjmnddja7z3aqy2vc7s01dyzikv2d38jsk4xzv5s";
+    url = https://github.com/NixOS/nixpkgs/archive/171e081b7288fce14fdea43ee0ce25c88738ab4d.tar.gz;
+    sha256 = "0ba7j33f42cnsg7g0812ld31w9mbf47f0wwmilr1756yxsvli3y2";
   }
