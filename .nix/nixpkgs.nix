813d462f4c7e8dc6fd40448e0553fab3a6011440
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-11-20 21:33:33 +0100

    findlib 1.9.8

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/171e081b7288fce14fdea43ee0ce25c88738ab4d.tar.gz;
-    sha256 = "0ba7j33f42cnsg7g0812ld31w9mbf47f0wwmilr1756yxsvli3y2";
-  }
+         url = https://github.com/vbgl/nixpkgs/archive/d8cafcfc6190c25cd039b0a19a6791af1cd0d05a.tar.gz;
+         sha256 = "1rvz15rwd9n0myg5dckjvw9ak0zydlycp3129ncz3bwk3ij13bmy";
+       }
