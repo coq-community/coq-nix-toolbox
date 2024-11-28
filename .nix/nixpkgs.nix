349d8642c215771de0028d2017e556f47f33d90b
Vincent Laporte <vbgl@users.noreply.github.com>
2024-11-28 09:10:37 +0100

    Ordinal 0.5.3 (#291)
    Update after NixOS/nixpkgs#334904

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/470e6e641f412894086b6df2f204847bd3905f17.tar.gz;
-    sha256 = "196l018hbmykm6bg82mnzp9qdbcqjm3rhiia4qsvybxd90f1dbb3";
+    url = https://github.com/NixOS/nixpkgs/archive/c78d3b84652b24b0094becef1b0f3aae4edd2553.tar.gz;
+    sha256 = "1ql2qr24v7dj9gjlc77zyhfbk17ly8h3g885i7aka5im4jd3sqjr";
   }
