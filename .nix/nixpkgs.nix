6d5a66175ad3292989865686cf1c816890cf8350
Cyril Cohen <cohen@crans.org>
2024-11-28 18:36:25 +0100

    Testing coqPackages.mathcomp: 2.2.0 -> 2.3.0

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/d337b780d0469c31c8bc756be3ee52240a3c2f4b.tar.gz";
-    sha256 = "0m9lh7024qb3pppsn1hnk9r4ifgiwd7s365fdza145vngd5nb2na";
-  }
+         url = "https://github.com/proux01/nixpkgs/archive/a8c45c71cbc49860a5f3819265ce24a6c0835352.tar.gz";
+         sha256 = "0gh3xwxya2khd9f5k3csxh8irsp1qaqni99n2zcjwyprgdhpl31a";
+       }
