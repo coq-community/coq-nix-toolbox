4936098eb19f182154ec029c3ad80ace3538b625
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-12-06 10:52:42 +0100

    elpi 2.0.5

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/c702d50a632d36226b302bfa312aacdfc0c9706f.tar.gz";
-    sha256 = "0zwl32ka51i97yz7b4vir2vb4xbcpmd0irwyayjgm93lly2v2ygh";
-  }
+         url = "https://github.com/vbgl/nixpkgs/archive/c9f7ce81c74a09729837bef339881919d6e618e0.tar.gz";
+         sha256 = "1i3a586pp0gqrmri2fhpf2q6hdxk3vwcgd93gnm14g0qazrpsc13";
+       }
