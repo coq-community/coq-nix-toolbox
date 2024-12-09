52dacbe8d836ce8111312b857a3f662fdcb2df7d
Cyril Cohen <cohen@crans.org>
2024-12-09 23:11:25 +0100

    testing hb 1.7.1

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/c702d50a632d36226b302bfa312aacdfc0c9706f.tar.gz";
-    sha256 = "0zwl32ka51i97yz7b4vir2vb4xbcpmd0irwyayjgm93lly2v2ygh";
-  }
+         url = "https://github.com/r-ryantm/nixpkgs/archive/44b47d6e000ee9312f8b45e9e688e0215934985a.tar.gz";
+         sha256 = "1ihqdpxqfzyydg640hlhxjvq2bi4cx84s92yq2fllrnxg8j9hwr8";
+       }
