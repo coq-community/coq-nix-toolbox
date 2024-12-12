13785ca796a883ca3f2fc1c1a1daa0ba8cea9ff2
Vincent Laporte <vbgl@users.noreply.github.com>
2024-12-12 10:48:09 +0100

    elpi 2.0.5 (#298)
    Update after NixOS/nixpkgs#362379

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/94b321638afed597e9bb528d45441c3fbfee2662.tar.gz";
-    sha256 = "0bc9rgqh4dcy40yibvp6qvsf4sbmikabli0mibf7qm58d2x80995";
+    url = "https://github.com/NixOS/nixpkgs/archive/31356d77aa87f6b2383f69447e16e4855acb2a40.tar.gz";
+    sha256 = "07bafgs1v4jz32lb30r92d57xzrr30137g7zbgnzq9wqssss2zk7";
   }
