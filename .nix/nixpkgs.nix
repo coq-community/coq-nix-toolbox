f6a4370ed3bcedb53835d9472dd058831918b8ef
Vincent Laporte <vbgl@users.noreply.github.com>
2024-12-12 06:55:51 +0100

    ExtLib 0.13.0 (#297)
    Update after NixOS/nixpkgs#362336

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/c702d50a632d36226b302bfa312aacdfc0c9706f.tar.gz";
-    sha256 = "0zwl32ka51i97yz7b4vir2vb4xbcpmd0irwyayjgm93lly2v2ygh";
+    url = "https://github.com/NixOS/nixpkgs/archive/94b321638afed597e9bb528d45441c3fbfee2662.tar.gz";
+    sha256 = "0bc9rgqh4dcy40yibvp6qvsf4sbmikabli0mibf7qm58d2x80995";
   }
