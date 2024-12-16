6a7a3a7aa222cb282d1641406c6d00917235b45d
Pierre Roux <pierre.roux@onera.fr>
2024-12-16 09:25:12 +0100

    coqPackages.coq-elpi: 2.0.5 -> 2.0.6
    Following merge of https://github.com/NixOS/nixpkgs/pull/364946

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/629830c8ffc7d1b34533a6ebebde0eaec0c5c941.tar.gz";
-    sha256 = "0m0jbfww57lk45cd1rsv0v112ybxiwhqpqwmw73wixq2r3r30ki6";
+    url = "https://github.com/NixOS/nixpkgs/archive/d337b780d0469c31c8bc756be3ee52240a3c2f4b.tar.gz";
+    sha256 = "0m9lh7024qb3pppsn1hnk9r4ifgiwd7s365fdza145vngd5nb2na";
   }
