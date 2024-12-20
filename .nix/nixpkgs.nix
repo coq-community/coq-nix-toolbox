a385ad99dc081abc4b374242eb9051e949800019
Pierre Roux <pierre.roux@onera.fr>
2024-12-20 18:11:55 +0100

    coq: keep compiling master
    Following merge of https://github.com/NixOS/nixpkgs/pull/367074

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/d337b780d0469c31c8bc756be3ee52240a3c2f4b.tar.gz";
-    sha256 = "0m9lh7024qb3pppsn1hnk9r4ifgiwd7s365fdza145vngd5nb2na";
+    url = "https://github.com/NixOS/nixpkgs/archive/bb881a68c1174d38ee466a35f5e23d03a938b186.tar.gz";
+    sha256 = "0fjnh93vvs30h1iw2ardil57aaz5fhgv504a9dcfpzdgqw4lds1n";
   }
