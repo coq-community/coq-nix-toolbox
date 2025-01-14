88ac05336e39e91fe204f07e6a984f26d1fd7034
Vincent Laporte <Vincent.Laporte@gmail.com>
2025-01-14 13:39:55 +0100

    paco 4.2.2

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/0bda27c93d0ba6ef5fae7e916f4c4691571e0471.tar.gz";
-    sha256 = "1cah2rsz4xfxab3y8dkp7x7xan7k1ppqn0maivv45753dy3jdbsb";
-  }
+         url = "https://github.com/vbgl/nixpkgs/archive/4f6ebaa6cd82d54638f5954bafe5ff5312284e34.tar.gz";
+         sha256 = "1b252fhqbcb1zs9dngzipm5kixg2266sww1aybmw4mf07s25ss49";
+       }
