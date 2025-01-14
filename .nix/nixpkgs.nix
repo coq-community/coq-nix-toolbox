eebef61dfafac82db297e0ca155e3470612be032
Vincent Laporte <Vincent.Laporte@inria.fr>
2025-01-14 11:43:14 +0100

    elpi 2.0.7

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/ac8941d77fc7945fd5addc021fe9a93f1a27d09c.tar.gz";
-    sha256 = "1z14lx03na0q59rhidasnsx3g3r3y0fxay2q4l4pg6lnny8547xy";
-  }
+         url = "https://github.com/gares/nixpkgs/archive/010306187751dd652e004299e5aba7fdb5a2c89e.tar.gz";
+         sha256 = "1i0hfd39lp7q6d3mv3lcq0angixls0hipww1cqlk71q4yiaps8l9";
+       }
