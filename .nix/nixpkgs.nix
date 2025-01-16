a24e6f4fb01d4ca0811dc8d16246cfe36b37ac52
Pierre Roux <pierre.roux@onera.fr>
2025-01-16 20:33:19 +0100

    Test coq: keep compiling master (#309)

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/f84e2fa942d3098ab31bda444e41061acdf9028c.tar.gz";
-    sha256 = "09v029axqqf8fcgy09qvnbxzpr58343yh7pa8k01m3z82rr4ha5h";
+    url = "https://github.com/NixOS/nixpkgs/archive/0bda27c93d0ba6ef5fae7e916f4c4691571e0471.tar.gz";
+    sha256 = "1cah2rsz4xfxab3y8dkp7x7xan7k1ppqn0maivv45753dy3jdbsb";
   }
