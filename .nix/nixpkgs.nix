c7e4038670ae0bc81c01f59ba006e7c29f2f7e29
Pierre Roux <pierre.roux@onera.fr>
2025-02-23 16:22:34 +0100

    coqPackages.mathcomp*: update stdlib dependencies
    Following merge of https://github.com/NixOS/nixpkgs/pull/384770

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/8fef72c519a075d563183c907f37433bb9c53f65.tar.gz";
-    sha256 = "1kd1yncbhcn27xpq1cwxgs1kbzxxsalyx19pfhz943x50r428n1w";
+    url = "https://github.com/NixOS/nixpkgs/archive/64ddb39f9909a0e09fa4398350d522e78f898290.tar.gz";
+    sha256 = "0zjllrs4rc47x086s5bxkvp3i5nmdp7493z8h4n3qg8pwmi1qvfr";
   }
