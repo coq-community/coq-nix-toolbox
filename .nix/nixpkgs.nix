cc66ac14394a6c3605b584b32688731d60a997ac
Vincent Laporte <vbgl@users.noreply.github.com>
2025-03-03 13:03:16 +0100

    CoqMatrix 1.0.6 (#333)
    Update after NixOS/nixpkgs#373307

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/aed9fca513162c496d58f09b3aa6ed6972bfb9df.tar.gz";
-    sha256 = "1kizqdh6k94nni73bhblsazkrpd4b9a7vkchphvgpk5ck23fh6hs";
+    url = "https://github.com/NixOS/nixpkgs/archive/9bc6260998b53825544192052f4cb9015fac9dc0.tar.gz";
+    sha256 = "1n0q7fww1iqzcj6c88hpxhy6zkwqn7w7kkcmnxvbbyqf727j5sm2";
   }
