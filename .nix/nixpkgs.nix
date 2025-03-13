bbc3b429134508f5640269521a483a2115fbbe1e
Pierre Roux <pierre.roux@onera.fr>
2025-03-13 09:28:41 +0100

    rocq-core: 9.0+rc1 -> 9.0.0

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/2a9c756f083874445dd088620173bf662b5ca0e4.tar.gz";
-    sha256 = "0cs6fgz5g82pn0kq85lkkmqyrwjrzqdqhzxgkms8jxccfqmaq7zw";
-  }
+         url = "https://github.com/proux01/nixpkgs/archive/0be8d64ca71d6ee9f5fb8442631334f4c4ef6c21.tar.gz";
+         sha256 = "0jqhsd4aaimfizk0dq9sh9frh9d9r41f5323d4maa4cmf0slry2y";
+       }
